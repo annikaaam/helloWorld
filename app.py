d5c388cc7dc7c91fce9fa21c7789c74baae4c4a6
annikaam <113716192+annikaam@users.noreply.github.com>
2024-03-26 09:20:33 -0400

    added route to contact and favorite course pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,19 @@ def hello():  # put application's code here
 @app.route('/about-css')
 def about():  # put application's code here
     return render_template('about-css.html')
+
+@app.route('/favorite-course')
+def favoriteCourse():  # put application's code here
+    print('Subject entered: ' + request.args.get('subject'))
+    print('Course number entered: ' + request.args.get('course_number'))
+    return render_template('favorite-course.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():  # put application's code here
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 
 if __name__ == '__main__':
